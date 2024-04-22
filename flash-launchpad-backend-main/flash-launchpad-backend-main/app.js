8492c7f64ec8883b8369f652a5d4a832a91ae6da
Jayraj Kapadia <jayraj@secretmindtech.com>
2024-04-22 17:48:38 +0530

    updated app.js

--- flash-launchpad-backend-main/flash-launchpad-backend-main/app.js
+++ flash-launchpad-backend-main/flash-launchpad-backend-main/app.js
@@ -14,14 +14,14 @@ var dbPath = process.env.DB_URL;
 
 // import the data layer
 var mongoose = require("mongoose");
-const  dashboard  = require("./application/controller/dashboard");
+const dashboard = require("./application/controller/dashboard");
 // import the models
 var models = {
   User: require("./application/model/user")(mongoose),
   Action: require("./application/model/action")(mongoose),
   Visitor: require("./application/model/visitor")(mongoose),
   Launchpad: require("./application/model/launchpad")(mongoose),
-  
+
   Notification: require("./application/model/notification")(mongoose),
   Token: require("./application/model/token")(mongoose),
 
@@ -31,8 +31,8 @@ var app = express();
 app.use(cors());
 // all environments
 app.set("port", process.env.PORT || 3000);
-app.set("views", __dirname + "/views");
-app.set("view engine", "jade");
+// app.set("views", __dirname + "/views");
+// app.set("view engine", "jade");
 app.use(express.favicon());
 app.use(express.logger("dev"));
 app.use(express.bodyParser());
